7db65d497ecde7407b0fb1fe55757212f54416ce
sookyeong <sookyeong0505@gmail.com>
2024-12-26 15:40:24 +0900

    네비게이션 스니펫 만들어서 include 태그를 이용해 적용함

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # 동적 경로 생성
-    path("", views.index),  # /challenges/
+    path("", views.index, name="index"),  # /challenges/
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
